8f892b871edbe06b4f86181eb8b5c631abf2eca7
zayanahmed925 <rjraju925@gmail.com>
2022-06-30 23:59:52 +0600

    task get and post added

--- index.js
+++ index.js
@@ -12,10 +12,34 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://taskList:<password>@cluster0.rvxbm.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvxbm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const tasksCollections = await client.db('task-list').collection('tasks');
+        //get all task
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const results = await tasksCollections.find(query).toArray();
+            res.send(results);
+        })
+        //post task
+        app.post('/tasks', async (req, res) => {
+            const data = req.body;
+            const result = await tasksCollections.insertOne(data);
+            res.send(result);
+        });
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
